4be6b2c996e95c9ada8d717eac5202c8eeba0057
Rondinele Guimaraes <rondinele.guimaraes@rerum.com.br>
2022-08-31 12:47:24 -0300

    Projeto validação de cpfs

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+var listaCPFS = new List<CPFValidator>
+{
+    new CPFValidator("634.449.030-65"),
+    new CPFValidator("021.215.080-84"),
+    new CPFValidator("57724756066"),
+    new CPFValidator("01782619046"),
+    new CPFValidator("222993590955455"),
+    new CPFValidator("999999999999"),
+    new CPFValidator("dsdsds"),
+};
+
+foreach(var cpf in listaCPFS)
+{
+   Console.WriteLine(cpf.IsValid());
+}
+
+
+
